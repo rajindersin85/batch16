166cd22f23792e8bef5b13b7bee426ac92b5ac11
M Kashif <mk.engineer3@yahoo.com>
2024-11-10 15:07:15 +0000

    kashif 3rd rg

--- main.tf
+++ main.tf
@@ -7,3 +7,8 @@ resource "azurerm_resource_group" "kashif" {
   name     = "kashif"
   location = "central india"
 }
+
+resource "azurerm_resource_group" "b16g18" {
+  name     = "rgb16g18"
+  location = "kashif"
+}
